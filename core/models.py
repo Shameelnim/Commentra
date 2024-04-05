3ef1915a55832c0cdc66e979a80deb46d4b6868a
PS4SPIDEY <psspidey4@gmail.com>
2024-04-05 06:20:33 +0530

    The total coding changed

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+	name = models.CharField(max_length=200)
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
