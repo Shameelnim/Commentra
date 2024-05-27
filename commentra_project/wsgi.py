71e8088443a4dcbc343dd21b9f9139feaebd8d61
SHAMEEL NIM <132083087+Shameelnim@users.noreply.github.com>
2024-05-27 13:15:30 +0530

    Update wsgi.py

--- commentra_project/wsgi.py
+++ commentra_project/wsgi.py
@@ -3,5 +3,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commentra_project.settings')
-
-application = get_wsgi_application()
+app = get_wsgi_application()
